8f22d582c0202787ae74a28188da2fbccb849484
Akash Dasgupta <akashdg09@gmail.com>
2020-01-17 17:07:20 +0000

    Streamlined loads: Under new scheme 2 update functions here, one for bulk and one for top/bottom  pml region. Independant of boundry, this is handeled in main

--- PML_boundry.cpp
+++ PML_boundry.cpp
@@ -13,15 +13,12 @@ void sigma_setter(double sigma_x[], double sigma_y[], int pml_size, double dt)
     for(int i=0; i<pml_size; ++i)
     {
         double factor{static_cast<double>(i+1) / static_cast<double>(pml_size)};
-        //std::cout << factor << std::endl;
-
         sigma_x[i] = epsilon_naught * (factor*factor* factor) / (2 * dt);
-        //std::cout << sigma_x[i] << std::endl;
     }
     
     for(int i=0; i<pml_size; ++i)
     {
-        double factor{static_cast<double>(i / pml_size)};
+        double factor{static_cast<double>(i+1) / static_cast<double>(pml_size)};
         sigma_y[i] = epsilon_naught * (factor*factor* factor) / (2 * dt);
     }
 }
@@ -36,36 +33,10 @@ void param_setter_x(PML_coefs Hx[], PML_coefs Hy[], PML_coefs Dz[], double sigma
     }
 }
 
-void param_setter_ytop(PML_coefs Hx[], PML_coefs Hy[], PML_coefs Dz[], double sigma_x[], double sigma_y[],  double mu_x, double mu_y, double dt, int pml_size, int Nx)
-{
-    for(int i=0; i<pml_size; ++i)
-    {
-        for(int j=0; j<pml_size; ++j)
-        {
-            Hx[index(i,j,Nx)].init_coefs_Hx(sigma_x[pml_size-(j+1)], sigma_y[pml_size-(i+1)], mu_x, dt);
-            Hy[index(i,j,Nx)].init_coefs_Hy(sigma_x[pml_size-(j+1)], sigma_y[pml_size-(i+1)], mu_y, dt);
-            Dz[index(i,j,Nx)].init_coefs_Dz(sigma_x[pml_size-(j+1)], sigma_y[pml_size-(i+1)], dt);
-        }
-        
-        for(int j=pml_size; j<Nx-pml_size; ++j)
-        {
-            Hx[index(i,j,Nx)].init_coefs_Hx(0, sigma_y[pml_size-(i+1)], mu_x, dt);
-            Hy[index(i,j,Nx)].init_coefs_Hy(0, sigma_y[pml_size-(i+1)], mu_y, dt);
-            Dz[index(i,j,Nx)].init_coefs_Dz(0, sigma_y[pml_size-(i+1)], dt);
-        }
-        
-        for(int j=Nx-pml_size; j<Nx; ++j)
-        {
-            Hx[index(i,j,Nx)].init_coefs_Hx(sigma_x[j-Nx+pml_size], sigma_y[pml_size-(i+1)], mu_x, dt);
-            Hy[index(i,j,Nx)].init_coefs_Hy(sigma_x[j-Nx+pml_size], sigma_y[pml_size-(i+1)], mu_y, dt);
-            Dz[index(i,j,Nx)].init_coefs_Dz(sigma_x[j-Nx+pml_size], sigma_y[pml_size-(i+1)], dt);
-        }
-    }
-}
 
-void param_setter_ybottom(PML_coefs Hx[], PML_coefs Hy[], PML_coefs Dz[], double sigma_x[], double sigma_y[],  double mu_x, double mu_y, double dt, int pml_size, int Nx)
+void param_setter(PML_coefs Hx[], PML_coefs Hy[], PML_coefs Dz[], double sigma_x[], double sigma_y[],  double mu_x, double mu_y, double dt, int pml_size,int y_size, int Nx)
 {
-    for(int i=0; i<pml_size; ++i)
+    for(int i=0; i<y_size; ++i)
     {
         for(int j=0; j<pml_size; ++j)
         {
