1dcd93be5556f6a0f704a3457c3b1f23fc0eb179
Akash Dasgupta <akashdg09@gmail.com>
2020-01-19 08:32:18 +0000

    removed redundant files

--- include/update_func_metalic.h
+++
@@ -1,8 +0,0 @@
-#ifndef UPDATE
-#define UPDATE
-
-void update_H(Point space[], int &Nx, int &Ny, double mux[],double muy[], double dx, double dy);
-void update_E(Point space[], int &Nx, int &Ny, double ep[], double dx, double dy, double dt);
-int index(int i,int j, int Nx);
-
-#endif
