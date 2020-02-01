c986a60ddb5a3ddc58372ee76c50e903e914315e
Akash Dasgupta <akashdg09@gmail.com>
2020-02-01 17:03:35 +0000

    Updated to take arrays

--- include/update_func_PML.h
+++ include/update_func_PML.h
@@ -2,16 +2,16 @@
 #define UPDATE
 
 // Updates magnetic field on all points of simulation space, for region within the 'top' PML
-void update_H_pml(Point space[], int Nx, int Ny, double dx, double dy, PML_coefs coefs_Hx[], PML_coefs coefs_Hy[], double ICHx[], double ICHy[]);
+void update_H_pml(double Ez[], double Hx[], double Hy[], int Nx, int Ny, double dx, double dy, PML_coefs coefs_Hx[], PML_coefs coefs_Hy[], double ICHx[], double ICHy[]);
 
 // Updates electric field on all points of simulation space, for region within the 'top' PML 
-void update_E_pml(Point space[], int Nx, int Ny, double ep[], double dx, double dy, double dt, PML_coefs coefs_Dz[], double IDz[]);
+void update_E_pml(double Ez[], double Dz[], double Hx[], double Hy[], int Nx, int Ny, double ep[], double dx, double dy, double dt, PML_coefs coefs_Dz[], double IDz[]);
 
 // Updates magnetic field on all points of simulation space, outside region of the 'top' and 'bottom' PML
-void update_H_bulk(Point space[], int Nx, int Ny, double mux[], double muy[], double dx, double dy, int pml_size, PML_coefs coefs_Hx[], PML_coefs coefs_Hy[], double ICHx[], double ICHy[]);
+void update_H_bulk(double Ez[], double Hx[], double Hy[], int Nx, int Ny, double mux[], double muy[], double dx, double dy, int pml_size, PML_coefs coefs_Hx[], PML_coefs coefs_Hy[], double ICHx[], double ICHy[]);
 
 // Updates electric field on all points of simulation space, outside region of the 'top' and 'bottom' PML
-void update_E_bulk(Point space[], int Nx, int Ny, double ep[], double dx, double dy, double dt,int pml_size, PML_coefs coefs_Dz[], double IDz[]);
+void update_E_bulk(double Ez[], double Dz[], double Hx[], double Hy[], int Nx, int Ny, double ep[], double dx, double dy, double dt,int pml_size, PML_coefs coefs_Dz[], double IDz[]);
 
 
 #endif
