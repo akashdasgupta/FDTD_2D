0c73356fd11976f8a398f9a712146434bb2ccbc6
Akash Dasgupta <akashdg09@gmail.com>
2020-02-10 13:35:20 +0000

    Added comments. Updated prototypes for less variables

--- include/soft_source.h
+++ include/soft_source.h
@@ -1,12 +1,9 @@
 #ifndef SOFTSOURCE
 #define SOFTSOURCE
-
 #include <math.h>  
-# include <cmath>
-
-void inject_soft_source(double Ez[], double Hx[], double Hy[], int iterations, double dx, double dy, double dt, double nsrc, double ersrc, double muxsrc, double muysrc, double sigma, double t0);
-
-void inject_soft_source2(double Ez[], double Hx[], double Hy[], int iterations, double dx, double dy, double dt, double nsrc, double ersrc, double muxsrc, double muysrc, double sigma, double t0);
-
-
+#include <cmath>
+//Creates a gausian pulse pertubation in the electric field
+void inject_soft_source(double Ez[], int iterations, double dt, double sigma, double t0);
+// Creates a contimous sin wave
+void inject_soft_source2(double Ez[], int iterations, double dt, double sigma, double t0);
 #endif
